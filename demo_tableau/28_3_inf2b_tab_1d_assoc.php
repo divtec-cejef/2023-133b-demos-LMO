4668906debcc707ea47393547ab462a096797db5
Laurent Moine <lam9@users.noreply.github.com>
2023-03-28 12:21:08 +0200

    ajout exemples tab 2d et parcours

--- demo_tableau/28_3_inf2b_tab_1d_assoc.php
+++ demo_tableau/28_3_inf2b_tab_1d_assoc.php
@@ -1,7 +1,7 @@
 <?php
 
 // Syntaxe courte.
-$tab2 = [
+/*$tab2 = [
     'id'          => 1001,
     'nom'         => 'Jura',
     'abreviation' => 'JU',
@@ -18,8 +18,59 @@ endforeach;
 foreach ($tab2 as $id => $value):
     /*var_dump($cle);
     var_dump($value);*/
-    echo "<p>$id - $value </p>";
+    //echo "<p>$id - $value </p>";
 
-endforeach;
+//endforeach;*/
 
 
+$cars = [
+    [
+        "marque"      => "Volvo",
+        "profondeur"  => 22,
+        "hauteur"     => 18
+    ],
+    [
+        "marque"      => "BMW",
+        "profondeur"  => 15,
+        "hauteur"     => 13
+    ],
+    [
+        "marque" => "Saab",
+        "profondeur" => 5,
+        "hauteur" => 2
+    ],
+    [
+        "marque" => "Land Rover",
+        "profondeur" => 17,
+        "hauteur" => 18
+    ]
+
+];
+
+var_dump($cars);
+
+//lecture et écriture
+var_dump( $cars[0]); // affiche une voiture
+var_dump( $cars[3]['marque']); // affiche la marque de la voiture 3
+
+
+//ajout d'une ligne
+$cars[]= [
+    "marque" => "Tesla",
+    "profondeur" => 17,
+    "hauteur" => 18
+];
+var_dump($cars);
+
+//parcourir
+foreach ($cars as $car) {
+    var_dump($car);
+    //var_dump($car['marque']);
+}
+
+foreach ($cars as $car){
+    //var_dump($car);
+    foreach ($car as $value){
+        var_dump($value);
+    }
+}
