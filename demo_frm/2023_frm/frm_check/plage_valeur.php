c9ad27cc993dc5c1d0e499398fefd414995aecc4
Laurent Moine <lam9@users.noreply.github.com>
2023-05-09 07:32:28 +0200

    maj demo frm

---
+++ demo_frm/2023_frm/frm_check/plage_valeur.php
@@ -0,0 +1,52 @@
+<?php
+// traitement
+
+
+// traitement du formulaire si'il est envoyé
+if(filter_input(INPUT_POST,'btnEnvoi') !== null){
+
+
+    //champ obligatoire
+    $val1 = filter_input(INPUT_POST, 'tbx1', FILTER_VALIDATE_INT);
+    var_dump($val1);
+    if($val1 ===''){
+        var_dump('erreur: champ obligatoire');
+    } else{
+        var_dump($val1);
+        if($val1 <=0){
+            var_dump('erreur: Hors plage');
+        }
+    }
+
+
+}
+?>
+
+
+<!--formulaire html-->
+<!doctype html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<!--l'action est sur la même page-->
+<form action="" method="post">
+    <p>
+        <label for="tbx1">saisir un nombre positif *</label>
+        <input type="text" name="tbx1"  id="tbx1">
+    </p>
+
+
+    <p>
+        <button type="submit" id="btn" name="btnEnvoi" value="btnEnvoi">Envoyer</button>
+    </p>
+
+</form>
+</body>
+</html>
