f049ea374345f4ca9fd7c0f53e3b0df14d69f7c6
Laurent Moine <lam9@users.noreply.github.com>
2023-05-09 11:49:44 +0200

    demo 9.5

--- demo_frm/2023_frm/frm_check/plage_valeur.php
+++ demo_frm/2023_frm/frm_check/plage_valeur.php
@@ -19,6 +19,15 @@ if(filter_input(INPUT_POST,'btnEnvoi') !== null){
     }
 
 
+    //plage de 100 à 200
+    $val1 = filter_input(INPUT_POST, 'tbx1', FILTER_VALIDATE_INT,[
+        "options" => [
+            "min_range"=>100,
+            "max_range"=>200
+        ]
+    ]);
+    var_dump($val1);
+
 }
 ?>
 
