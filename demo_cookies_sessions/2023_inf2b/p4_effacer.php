2f8cc17aba5d71e25b3514545eed4527eacfa37d
Laurent Moine <lam9@users.noreply.github.com>
2023-05-23 12:19:05 +0200

    23.5.23

--- demo_cookies_sessions/2023_inf2b/p4_effacer.php
+++ demo_cookies_sessions/2023_inf2b/p4_effacer.php
@@ -2,12 +2,16 @@
 //démarrer la session PHP
 session_start();
 
-//efface le fichier de session sur le serveur
-session_destroy();
+
 
 //efface le cookie sur le navigateur
+setcookie (session_id(), "", time() - 3600);
 
 
+
+//efface le fichier de session sur le serveur
+session_destroy();
+session_write_close();
 //header('location:p1_fr.php');
 
 
